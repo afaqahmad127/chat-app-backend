efc7bb871a12abd703cbcbe2b790d41858a311a5
afaqahmad127 <you@example.com>
2022-09-29 14:18:24 +0500

    Pushed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import {} from "dotenv/config";
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import loaders from "./loaders/index.js";
 import config from "./config/index.js";
